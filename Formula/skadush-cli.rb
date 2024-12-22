9a28c5b16007e9d5dca6d4e36c05d926b534c278
Minenhle Sithole <minenhle@skadush.co.za>
2024-12-22 18:32:24 +0200

    test formulae

---
+++ Formula/skadush-cli.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Mytool < Formula
+    desc "Skadush's super secret aliases"
+    homepage "https://https://github.com/mskadush"
+    url "https://github.com/mskadush/skadush-cli/blob/main/app/cli-1.1-SNAPSHOT.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    # sha256 "<SHA256 CHECKSUM>"
+    head "https://github.com/mskadush/skadush-cli.git"
+  
+    def install
+        # build/distributions/cli-1.1-SNAPSHOT/bin/cli
+      bin.install "bin/cli" => "skadush-cli"
+    end
+  
+    # Homebrew requires tests.
+    test do
+      assert_match "version: 1.x.x", shell_output("#{bin}/skadush-cli", 0)
+    end
+  end
