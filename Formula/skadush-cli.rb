207a2570d5c058abc7c33b02e8b6e62a00e37b11
Minenhle Sithole <minenhle@skadush.co.za>
2024-12-22 19:22:50 +0200

    fixing extraction

--- Formula/skadush-cli.rb
+++ Formula/skadush-cli.rb
@@ -4,13 +4,13 @@ require_relative "../custom_download_strategy.rb"
 class SkadushCli < Formula
     desc "Skadush's super secret aliases"
     homepage "https://https://github.com/mskadush"
-    url "https://github.com/mskadush/skadush-cli/blob/main/app/cli-1.0.0.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+    url "https://github.com/mskadush/skadush-cli/archive/refs/tags/1.0.1.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
     # sha256 "<SHA256 CHECKSUM>"
     head "https://github.com/mskadush/skadush-cli.git"
   
     def install
         # build/distributions/cli-1.1-SNAPSHOT/bin/cli
-      bin.install "bin/cli" => "skadush-cli"
+        bin.install "app/cli-1.1-SNAPSHOT/scripts/cli" => "skadush-cli"
     end
   
     # Homebrew requires tests.
