4e3bb714cfdfbffe96044592d20aeb1f37911d1d
Minenhle Sithole <minenhle@skadush.co.za>
2024-12-22 18:43:44 +0200

    remove release strat

--- Formula/skadush-cli.rb
+++ Formula/skadush-cli.rb
@@ -4,7 +4,7 @@ require_relative "../custom_download_strategy.rb"
 class Mytool < Formula
     desc "Skadush's super secret aliases"
     homepage "https://https://github.com/mskadush"
-    url "https://github.com/mskadush/skadush-cli/blob/main/app/cli-1.1-SNAPSHOT.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/mskadush/skadush-cli/blob/main/app/cli-1.1-SNAPSHOT.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
     # sha256 "<SHA256 CHECKSUM>"
     head "https://github.com/mskadush/skadush-cli.git"
   
